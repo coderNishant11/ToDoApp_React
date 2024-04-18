963443dd32074f465c0f2f14fc895119899b6511
coderNishant11 <mr.nishantksrivastava@gmail.com>
2024-04-18 14:02:51 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import ToDoHeading from './components/ToDoHeading'
+
+function App() {
+
+
+  return (
+    <div className="todo-app-container">
+      <ToDoHeading/>
+    </div>
+  
+  )
+}
+
+export default App
---
+++ src/components/ToDoHeading.jsx
@@ -0,0 +1,5 @@
+const ToDoHeading = () => {
+    return <h1>My Todos</h1>
+}
+
+export default ToDoHeading;
